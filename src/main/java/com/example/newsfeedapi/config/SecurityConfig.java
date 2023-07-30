51fbab0d15ec6d2571e5343ee0fcf3cc8d76ee18
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-07-30 16:23:19 +0700

    #code

--- src/main/java/com/example/newsfeedapi/config/SecurityConfig.java
+++ src/main/java/com/example/newsfeedapi/config/SecurityConfig.java
@@ -25,6 +25,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
