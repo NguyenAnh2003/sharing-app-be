51f21e706ade14d8206e8edb39975415e38bbe08
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-09-01 18:56:06 +0700

    #code

--- src/main/java/com/example/socialapi/common/MyCustomExceptions.java
+++ src/main/java/com/example/socialapi/common/MyCustomExceptions.java
@@ -1,10 +1,16 @@
 package com.example.socialapi.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-public enum MyCustomExceptions {
+@AllArgsConstructor
+@Getter
+@Setter
+public class MyCustomExceptions {
+    private WebStatus webStatus;
+    // https://www.baeldung.com/a-guide-to-java-enums
 
 }
--- src/main/java/com/example/socialapi/common/WebStatus.java
+++ src/main/java/com/example/socialapi/common/WebStatus.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum WebStatus {
-    UNAUTHORIZEd(403, HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(403, HttpStatus.UNAUTHORIZED),
     NOT_FOUND(404, HttpStatus.NOT_FOUND);
 
     private final int status;
