9d82158a6754a309c835912c58df247900a5fa4e
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-01-02 11:44:49 +0700

    #code

--- src/main/java/com/example/socialapi/post/PostService.java
+++ src/main/java/com/example/socialapi/post/PostService.java
@@ -37,71 +37,100 @@ public class PostService {
     public PostDTO createPostService(String userId, String categoryId,
                                      String title, String description,
                                      String imageURL) {
-        logging.info("creating post in service");
-        Post post = new Post(userMapper.apply(userRepository.findUserById(new ObjectId(userId)).orElseThrow(() -> new UsernameNotFoundException("User not found"))),
-                categoryMapper.apply(cateRepository.findById(categoryId).orElseThrow()),
-                title,
-                description,
-                imageURL, LocalDateTime.now());
-        return mapper.apply(repository.save(post));
+        try {
+            logging.info("creating post in service");
+            Post post = new Post(userMapper.apply(userRepository.findUserById(new ObjectId(userId)).orElseThrow(() -> new UsernameNotFoundException("User not found"))),
+                    categoryMapper.apply(cateRepository.findById(categoryId).orElseThrow()),
+                    title,
+                    description,
+                    imageURL, LocalDateTime.now());
+            return mapper.apply(repository.save(post));
+        } catch (Exception e) {
+            logging.error("Internal error cannot create post");
+            throw new RuntimeException("Message " + e.getMessage() + " Cause " + e.getCause());
+        }
     }
 
     /* update */
     public PostDTO updatePostService(String id, String userId,
                                      String categoryId, String title,
                                      String description, String imageURL) {
-        logging.info("update post with postId", id);
-        Post post = repository.findById(id).orElseThrow();
-        post.setCategory(categoryMapper.apply(cateRepository.findById(categoryId).orElseThrow()));
-        post.setTitle(title);
-        post.setDescription(description);
-        post.setImageURL(imageURL);
-        // set
-        return mapper.apply(repository.save(post));
+        try {
+            logging.info("update post with postId", id);
+            Post post = repository.findById(id).orElseThrow();
+            post.setCategory(categoryMapper.apply(cateRepository.findById(categoryId).orElseThrow()));
+            post.setTitle(title);
+            post.setDescription(description);
+            post.setImageURL(imageURL);
+            return mapper.apply(repository.save(post));
+        } catch (Exception e) {
+            logging.error("Internal error cannot update post");
+            throw new RuntimeException("Message " + e.getMessage() + " Cause " + e.getCause());
+        }
     }
 
     /* read */
     public List<PostDTO> getAll(String userId) {
-        logging.info("get all posts by userId and followingUserId");
-        List<Post> listOfPosts = new ArrayList<Post>();
-        listOfPosts.clear();
-        // define posts array for result fetching
-        List<Post> posts = repository.findAllByUserId(userId).orElseThrow();
-        if(posts.isEmpty()) return Collections.emptyList();
-        else listOfPosts.addAll(posts); // add all posts fetched by userId
-        List<Post> followingUsersPosts = new ArrayList<Post>(); // following users posts
-        List<FollowDTO> followingUsers = followService.getFollowingUsers(userId); // get following users by userId
-        if(followingUsers.isEmpty()) return Collections.emptyList();
-        else {followingUsers.forEach(
-            follow -> {
-                followingUsersPosts.addAll(repository.findAllByUserId(String.valueOf(follow.getFollowingId())).orElseThrow());
-                listOfPosts.addAll(followingUsersPosts); // add to result
-                followingUsersPosts.clear(); // remove init array after adding
-            }
-        );}
-        return listOfPosts.isEmpty() ? Collections.emptyList() : listOfPosts.stream().map(mapper).collect(Collectors.toList());
+        try {
+            logging.info("get all posts by userId and followingUserId");
+            List<Post> listOfPosts = new ArrayList<Post>();
+            listOfPosts.clear();
+            // define posts array for result fetching
+            List<Post> posts = repository.findAllByUserId(userId).orElseThrow();
+            if(posts.isEmpty()) return Collections.emptyList();
+            else listOfPosts.addAll(posts); // add all posts fetched by userId
+            List<Post> followingUsersPosts = new ArrayList<Post>(); // following users posts
+            List<FollowDTO> followingUsers = followService.getFollowingUsers(userId); // get following users by userId
+            if(followingUsers.isEmpty()) return Collections.emptyList();
+            else {followingUsers.forEach(
+                    follow -> {
+                        followingUsersPosts.addAll(repository.findAllByUserId(String.valueOf(follow.getFollowingId())).orElseThrow());
+                        listOfPosts.addAll(followingUsersPosts); // add to result
+                        followingUsersPosts.clear(); // remove init array after adding
+                    }
+            );}
+            return listOfPosts.isEmpty() ? Collections.emptyList() : listOfPosts.stream().map(mapper).collect(Collectors.toList());
+        } catch (Exception e) {
+            logging.error("Internal error cannot get all posts");
+            throw new RuntimeException("Message " + e.getMessage() + " Cause " + e.getCause());
+        }
     }
 
     public PostDTO getPostById(String id) {
-        logging.info("get single post by postId");
-        return mapper.apply(repository.findById(id).orElseThrow());
+        try {
+            logging.info("get single post by postId");
+            return mapper.apply(repository.findById(id).orElseThrow());
+        } catch (Exception e) {
+            logging.error("Internal error cannot get post by postId");
+            throw new RuntimeException("Message " + e.getMessage() + " Cause " + e.getCause());
+        }
     }
 
     public List<PostDTO> getPostsByUserId(String userId) {
-        logging.info("get all posts by userId");
-        return repository.findAllByUserId(userId).orElseThrow()
-                .stream()
-                .map(mapper)
-                .collect(Collectors.toList());
+        try {
+            logging.info("get all posts by userId");
+            return repository.findAllByUserId(userId).orElseThrow()
+                    .stream()
+                    .map(mapper)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            logging.error("Internal error cannot get posts by userId");
+            throw new RuntimeException("Message " + e.getMessage() + " Cause " + e.getCause());
+        }
     }
 
     /* delete */
     public Boolean deletePost(String id) {
-        logging.info("deleting post", id);
-        Post p = repository.findById(id).orElseThrow();
-        repository.delete(p);
-        boolean existed = repository.existsById(id);
-        return existed;
+        try {
+            logging.info("deleting post", id);
+            Post p = repository.findById(id).orElseThrow();
+            repository.delete(p);
+            boolean existed = repository.existsById(id);
+            return existed;
+        } catch (Exception e) {
+            logging.error("Internal error cannot delete post");
+            throw new RuntimeException("Message " + e.getMessage() + " Cause " + e.getCause());
+        }
     }
 
 }
